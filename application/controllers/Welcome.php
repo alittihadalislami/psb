bf3c2936e5eb7456a7731cbbdfac488c4d7d8e13
Bismillah_123 <alittihadalislami@gmail.com>
2019-12-31 22:21:15 -0800

    halaman utama

--- application/controllers/Welcome.php
+++ application/controllers/Welcome.php
@@ -5,6 +5,6 @@ class Welcome extends CI_Controller {
 
 	public function index()
 	{
-		$this->load->view('utama/utama');
+		$this->load->view('utama/landing');
 	}
 }
--- application/views/utama/utama.php
+++ application/views/utama/utama.php
@@ -19,7 +19,7 @@
 <head>
   <meta charset="utf-8" />
   <link rel="apple-touch-icon" sizes="76x76" href="<?=base_url()?>assets/img/apple-icon.png">
-  <link rel="icon" type="image/png" href="../assets/img/favicon.png">
+  <link rel="icon" type="image/png" href="<?=base_url()?>assets/img/favicon.png">
   <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1" />
   <title>
     Material Kit by Creative Tim
@@ -29,9 +29,9 @@
   <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
   <!-- CSS Files -->
-  <link href="../assets/css/material-kit.css?v=2.0.6" rel="stylesheet" />
+  <link href="<?=base_url()?>assets/css/material-kit.css?v=2.0.6" rel="stylesheet" />
   <!-- CSS Just for demo purpose, don't include it in your project -->
-  <link href="../assets/demo/demo.css" rel="stylesheet" />
+  <link href="<?=base_url()?>assets/demo/demo.css" rel="stylesheet" />
 </head>
 
 <body class="login-page sidebar-collapse">
@@ -54,7 +54,7 @@
               <i class="material-icons">apps</i> Components
             </a>
             <div class="dropdown-menu dropdown-with-icons">
-              <a href="../index.html" class="dropdown-item">
+              <a href="<?=base_url()?>index.html" class="dropdown-item">
                 <i class="material-icons">layers</i> All Components
               </a>
               <a href="https://demos.creative-tim.com/material-kit/docs/2.1/getting-started/introduction.html" class="dropdown-item">
@@ -86,7 +86,7 @@
       </div>
     </div>
   </nav>
-  <div class="page-header header-filter" style="background-image: url('../assets/img/bg7.jpg'); background-size: cover; background-position: top center;">
+  <div class="page-header header-filter" style="background-image: url('<?=base_url()?>assets/img/bg7.jpg'); background-size: cover; background-position: top center;">
     <div class="container">
       <div class="row">
         <div class="col-lg-4 col-md-6 ml-auto mr-auto">
@@ -178,18 +178,18 @@
     </footer>
   </div>
   <!--   Core JS Files   -->
-  <script src="../assets/js/core/jquery.min.js" type="text/javascript"></script>
-  <script src="../assets/js/core/popper.min.js" type="text/javascript"></script>
-  <script src="../assets/js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
-  <script src="../assets/js/plugins/moment.min.js"></script>
+  <script src="<?=base_url()?>assets/js/core/jquery.min.js" type="text/javascript"></script>
+  <script src="<?=base_url()?>assets/js/core/popper.min.js" type="text/javascript"></script>
+  <script src="<?=base_url()?>assets/js/core/bootstrap-material-design.min.js" type="text/javascript"></script>
+  <script src="<?=base_url()?>assets/js/plugins/moment.min.js"></script>
   <!--	Plugin for the Datepicker, full documentation here: https://github.com/Eonasdan/bootstrap-datetimepicker -->
-  <script src="../assets/js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
+  <script src="<?=base_url()?>assets/js/plugins/bootstrap-datetimepicker.js" type="text/javascript"></script>
   <!--  Plugin for the Sliders, full documentation here: http://refreshless.com/nouislider/ -->
-  <script src="../assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
+  <script src="<?=base_url()?>assets/js/plugins/nouislider.min.js" type="text/javascript"></script>
   <!--  Google Maps Plugin    -->
   <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
   <!-- Control Center for Material Kit: parallax effects, scripts for the example pages etc -->
-  <script src="../assets/js/material-kit.js?v=2.0.6" type="text/javascript"></script>
+  <script src="<?=base_url()?>assets/js/material-kit.js?v=2.0.6" type="text/javascript"></script>
 </body>
 
 </html>
