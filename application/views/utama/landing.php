4702ea47eff76646602127c7cbfbfcf526def3b5
Bismillah_123 <alittihadalislami@gmail.com>
2020-01-02 20:49:12 +0700

    update landing download file pdf

--- application/views/utama/landing.php
+++ application/views/utama/landing.php
@@ -73,7 +73,7 @@
             </div>
           </li>
           <li class="nav-item">
-            <a class="nav-link" href="<?=base_url()?>assets/file/brosur_alittihad_2020-k.pdf" download>
+            <a class="nav-link" href="<?=base_url()?>assets/file/brosur_alittihad_2020-k.pdf" download="Brosur Ma'had Al Ittihad Al Islami">
               <i class="material-icons">cloud_download</i> Unduh Brosur
             </a>
           </li>
