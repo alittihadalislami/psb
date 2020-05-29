050d23f9fe58cf7808637c4f9408a2ecc2e336e0
Bismillah_123 <alittihadalislami@gmail.com>
2020-05-29 10:00:43 +0700

    Update unggahfile.php

--- application/views/login/unggahfile.php
+++ application/views/login/unggahfile.php
@@ -8,7 +8,7 @@
   		<?= $this->session->flashdata('pesan'); ?>
 
   		<div class="info small font-weight-bold">
-	  		<p>Sangat disarankan untuk <span style="font-style: italic;">menscan</span> file yang akan diupload dengan parangkat scanner dengan baik agar file bisa dibaca dengan baik.. </p>
+	  		<p>Sangat disarankan untuk <span style="font-style: italic;">menscan</span> file yang akan diupload dengan parangkat scanner yang baik, agar file bisa dibaca dengan baik.. </p>
 	  		<p>File yang bisa diupload type <strong class="text-success">gambar .png, .jpg dan berukuran maksimal 1MB</strong></p>
 	  		<p>Jika file terlalu besar, silahkan ubah ukuran filenya. bisa menggunakan aplikasi online seperti : <a href="https://compressjpeg.com" target="_blank">compressjpeg.com</a>, <a href="https://tinyjpg.com" target="_blank">tinyjpg.com</a>, atau yang lainnya.. </p>
   		</div>
