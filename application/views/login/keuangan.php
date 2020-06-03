aee6ba79d61f4d87aa9cec3b64eb367b329f1fb2
fu4d1 <hambaallah896@gmail.com>
2020-06-03 15:26:36 +0700

    update file keuangan

--- application/views/login/keuangan.php
+++ application/views/login/keuangan.php
@@ -4,12 +4,17 @@
 		<h5>Ma'had Al Ittihad - 2020</h5>
 	</div>
   	<div class="card-body rounded-0">
-  		
-  		<?= $this->session->flashdata('pesan'); ?>
 
   		<div class="row">
   			<div class="col-12 pb-5">
-  				<img src="<?= base_url('assets/img/biaya.jpg') ?>" width=100%>
+  				<a href="<?= base_url('assets/img/biaya.jpg') ?>"  
+				   onclick="window.open('<?= base_url('assets/img/biaya.jpg') ?>', 
+				                         'newwindow', 
+				                         'width=500,height=700'); 
+				   return false;" >
+				 	<img src="<?= base_url('assets/img/biaya.jpg') ?>" width=100%>
+				</a>
+  				
   			</div>
   		</div>
 
@@ -17,36 +22,45 @@
 	  			Dipersilahkan untuk membayar biaya sebagai tertera diatas. 
 	  				<form class="form-inline ml-0">
 					  <div class="form-group mb-2">
-					    <input type="text" value="Rp. 4.825.000" class="form-control form-control-sm bg-white text-dark" style="font-size: 30px" id="bri1" placeholder="Password" readonly="">
+					    <input type="text" value="Rp. 4.825.000" class="form-control form-control-sm bg-white text-dark" style="font-size: 30px; width: 220px" id="bri1" placeholder="Password" readonly="">
 					  </div>
-					  <button class="btn btn-danger btn-sm mb-2 mx-sm-1 d-block mx-auto">Salin</button>
+					  <button data-nilai="4825000" type="button" class="btn btn-danger btn-sm mb-2 mx-sm-1 d-block mx-auto copy">Salin</button>
 					</form>
 					<br>
 
-	  			Dengan mentransfer ke Rekening Bendahara Ma'had Al Ittihad Al Islami.
-		  			
-		  			Putra: BRI an. Muhammad Azhari :
+	  				Dengan mentransfer ke Rekening Bendahara Ma'had Al Ittihad Al Islami.
+		  			<br><img src="<?= base_url('assets/img/bri.png') ?>" style="height: 30px; margin-bottom: 10px; margin-top: 10px">
+		  			<strong>an. Muhammad Azhari</strong>
 	  				<form class="form-inline ml-0">
 					  <div class="form-group mb-2">
-					    <input type="text" value="3882-01-016-304-53-4" class="form-control  bg-white text-dark" style="font-size: 24px" id="bri1" placeholder="Password" readonly="">
+					    <input type="text" value="3882-01-016-304-53-4"  class="form-control  bg-white text-dark" style="font-size: 24px;" id="bri1" placeholder="Password" readonly>
 					  </div>
-					  <button class="btn btn-danger btn-sm mb-2 mx-sm-1">Salin</button>
+					  <div style="width: 42%" class="d-sm-none"></div>
+					  <button type="button" data-nilai="388201016304534" class="btn btn-danger btn-sm mb-2 mx-sm-1 copy">Salin</button>
 					</form>
-
-		  			<!-- Putri: BRI an. Mariatul Jannah : 
+					<br>
+					atau
+		  			<br><img src="<?= base_url('assets/img/bri.png') ?>" style="height: 30px; margin-bottom: 10px; margin-top: 10px">
+		  			<strong>an. Mariatul Jannah</strong>
 		  			<form class="form-inline ml-0">
 					  <div class="form-group mb-2">
-					    <input type="text" value="0148-01-065-705-50-4" class="form-control" id="bri1" style="font-size: 24px" readonly="">
+					    <input type="text" value="0148-01-065-705-50-4" class="form-control bg-white text-dark" id="bri1" style="font-size: 24px" readonly>
 					  </div>
-					  <button class="btn btn-danger btn-sm mb-2 mx-sm-1">Salin</button>
-					</form> -->
+					  <div style="width: 42%" class="d-sm-none"></div>
+					  <button type="button" data-nilai="014801065705504" class="btn btn-danger btn-sm mb-2 mx-sm-1 copy">Salin</button>
+					</form>
   		</div>
 
 
   		<div class="row">
   			<div class="col-md-8">
+  				
+  				<?= $this->session->flashdata('pesan'); ?>
+
   				<div class="row">
-	  				<div class="col-6 font-weight-bold">Upload bukti transfer</div>
+	  				<div class="col-12">
+	  					Dan mengunggah bukti transfer dengan form dibawah ini
+	  				</div>
   				</div>
   				<hr class="mt-1">
   			</div>
@@ -56,23 +70,13 @@
   				
   				$transfer = isset($data['keuangan']) ? base_url('uploads/transfer/').$data['keuangan'] : null;
 
-  				$transfer = null;
+  				// $transfer = null;
 
   				$display = $transfer == null ? 'd-none' : 'd-block';
   				$read = $transfer == null ? '' : 'readonly';
   				$disable = $transfer == null ? '' : 'disabled';
   			?>
 
-  			<div class="col-12 col-md-4 <?= $display ?>">
-  				<a href="<?= $transfer ?>"  
-				   onclick="window.open('<?= $transfer ?>', 
-				                         'newwindow', 
-				                         'width=500,height=700'); 
-				   return false;" >
-				 	<img class="img-thumbnail d-sm-block mx-auto" src="<?= $transfer ?>" style="height: 100px;">
-				</a>
-				
-  			</div>
 
   			<?php echo form_error('bukti-bayar'); ?>
 
@@ -85,16 +89,27 @@
 					</div>
 
 					<div class="form-group">
-					    <label for="id">ID calon santri</label>
+					    <label for="id">ID calon santri (otomatis dari sistem)</label>
 				    	<input type="text" class="form-control" name="id_data_awal" value="<?= $data['id_data_awal']?>" readonly>
 					</div>
 
-					<div class="custom-file">
+		  			<div class="<?= $display ?>">
+		  				<a href="<?= $transfer ?>"  
+						   onclick="window.open('<?= $transfer ?>', 
+						                         'newwindow', 
+						                         'width=500,height=700'); 
+						   return false;" >
+						 	<img class="img-thumbnail d-sm-block mx-auto" src="<?= $transfer ?>" style="height: 100px;">
+						</a>
+						
+		  			</div>
+
+					<div class="custom-file mt-3">
 					    <input type="file" required class="custom-file-input" name="bukti-bayar" id="bukti-bayar" <?= $read.' '.$disable ?> >
 					    <label class="custom-file-label" for="bukti-bayar">Upload bukti transfer</label>
 					</div>
 
-					<button class="btn btn-warning d-block mr-auto mt-4" <?= $disable ?> type="button">Simpan</button>
+					<button type="submit" class="btn btn-warning d-block mr-auto mt-4" <?= $disable ?> type="button">Simpan</button>
 				</form>
 			</div>
   		</div>
--- assets/js/my.js
+++ assets/js/my.js
@@ -86,6 +86,22 @@ $('.nav-link').on('click',function(){
 	
 });
 
+var ClipboardHelper = {
+
+    copyElement: function ($element)
+    {
+       this.copyText($element.text())
+    },
+    copyText:function(text) // Linebreaks with \n
+    {
+        var $tempInput =  $("<textarea>");
+        $("body").append($tempInput);
+        $tempInput.val(text).select();
+        document.execCommand("copy");
+        $tempInput.remove();
+    }
+};
+
 $(document).ready(function(){
 	
 	$('#simpanForm').on('click',function(){
@@ -104,7 +120,14 @@ $(document).ready(function(){
 	});
 
 	tampilPertanyaan();
+
+	$('.copy').on('click',function(){
+		x = $(this).data('nilai');
+		ClipboardHelper.copyText(x);
+	})
 });
+
+
 
 function tampilPertanyaan(param = 1){
 	pilihan_ganda = ''
@@ -137,10 +160,7 @@ $('#simpan-asesment').on('click', function(){
 	tampilPertanyaan(id);
 });
 
-// $('.btn-danger').on("click",function(){
-//     alert('x');
-//     // e.preventDefault();
-// });
+
 
 
 
